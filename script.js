69208edaeacd03918e9f99f7ef4e66667c768292
raaghav03 <raghavnagpal2003@gmail.com>
2024-03-04 04:22:02 +0530

    intial functionality 1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello inital js setup')
+const tasks = document.querySelectorAll(".tasks");
+
+tasks.forEach((task) => {
+  task.addEventListener("click", function () {
+    const titleElement = this.querySelector(".title");
+    const titleText = titleElement.textContent;
+    const detailsCardTitle = document.querySelector(
+      ".specific-details-card h1"
+    );
+    detailsCardTitle.textContent = titleText;
+  });
+});
