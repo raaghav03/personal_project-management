143418c1b09459e173a3804dedcd6b00f2cdd8c8
raaghav03 <raghavnagpal2003@gmail.com>
2024-03-06 00:33:27 +0530

    main content refine

--- script.js
+++ script.js
@@ -1,82 +1,95 @@
-const ul = document.querySelector("ul");
-ul.addEventListener("click", function (event) {
-  const taskClicked = event.target.closest(".tasks");
-  if (taskClicked) {
-    const titleElement = taskClicked.querySelector(".title");
-    const subtitleElement = taskClicked.querySelector(".subtitle");
+const clickedLi = document.querySelectorAll("li");
 
-    const subTitleText = subtitleElement ? subtitleElement.textContent : "";
-    const titleText = titleElement ? titleElement.textContent : "";
-    const detailCardSubtitle = document.querySelector(
-      ".specific-details-card h2"
-    );
-    const detailsCardTitle = document.querySelector(
-      ".specific-details-card h1"
-    );
-    detailsCardTitle.textContent = titleText;
-    detailCardSubtitle.textContent = subTitleText;
-  }
+
+clickedLi.forEach((li) => {
+  li.addEventListener("click", function () {
+    const text = li.textContent;
+    console.log(text);
+    const h1 = document.getElementsByClassName("main-content-h1")[0];
+    console.log(h1);
+    h1.textContent = text;
+  });
 });
 
-const addTask = document.querySelector(".addTask");
+// const ul = document.querySelector("ul");
+// ul.addEventListener("click", function (event) {
+//   const taskClicked = event.target.closest(".tasks");
+//   if (taskClicked) {
+//     const titleElement = taskClicked.querySelector(".title");
+//     const subtitleElement = taskClicked.querySelector(".subtitle");
 
-addTask.addEventListener("click", function (event) {
-  event.preventDefault();
-  // Retrieve input values
-  const title = document.getElementById("projectTitle").value;
-  const subtitle = document.getElementById("projectSubtitle").value;
-  let priority;
-  const radios = document.querySelectorAll('input[name="priorityLevel"]');
-  radios.forEach((radio) => {
-    if (radio.checked) {
-      priority = radio.value; // Make sure to get the value of the radio
-    }
-  });
+//     const subTitleText = subtitleElement ? subtitleElement.textContent : "";
+//     const titleText = titleElement ? titleElement.textContent : "";
+//     const detailCardSubtitle = document.querySelector(
+//       ".specific-details-card h2"
+//     );
+//     const detailsCardTitle = document.querySelector(
+//       ".specific-details-card h1"
+//     );
+//     detailsCardTitle.textContent = titleText;
+//     detailCardSubtitle.textContent = subTitleText;
+//   }
+// });
 
-  // Create the task elements
-  const li = document.createElement("li");
-  li.className = "tasks";
+// const addTask = document.querySelector(".addTask");
 
-  const todoListContainer = document.createElement("div");
-  todoListContainer.className = "todoListContainer";
+// addTask.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   // Retrieve input values
+//   const title = document.getElementById("projectTitle").value;
+//   const subtitle = document.getElementById("projectSubtitle").value;
+//   let priority;
+//   const radios = document.querySelectorAll('input[name="priorityLevel"]');
+//   radios.forEach((radio) => {
+//     if (radio.checked) {
+//       priority = radio.value; // Make sure to get the value of the radio
+//     }
+//   });
 
-  const titleSubtitle = document.createElement("div");
-  titleSubtitle.className = "title_subtitle";
+//   // Create the task elements
+//   const li = document.createElement("li");
+//   li.className = "tasks";
 
-  const pTitle = document.createElement("p");
-  pTitle.className = "title";
-  pTitle.textContent = title;
+//   const todoListContainer = document.createElement("div");
+//   todoListContainer.className = "todoListContainer";
 
-  const pSubtitle = document.createElement("p");
-  pSubtitle.className = "subtitle";
-  pSubtitle.textContent = subtitle;
-  document.querySelectorAll(".priorityLevel").forEach((div) => {
-    if (div.textContent === "high") {
-      div.className = "priorityLevelHigh";
-    } else {
-      div.className = "priorityLevelLow";
-    }
-  });
+//   const titleSubtitle = document.createElement("div");
+//   titleSubtitle.className = "title_subtitle";
 
-  const priorityDiv = document.createElement("div");
-  priority == "high"
-    ? (priorityDiv.className = "priorityLevelHigh")
-    : (priorityDiv.className = "priorityLevelLow");
-  priorityDiv.textContent = priority.replace("-", " ");
+//   const pTitle = document.createElement("p");
+//   pTitle.className = "title";
+//   pTitle.textContent = title;
 
-  titleSubtitle.appendChild(pTitle);
-  titleSubtitle.appendChild(pSubtitle);
-  todoListContainer.appendChild(titleSubtitle);
+//   const pSubtitle = document.createElement("p");
+//   pSubtitle.className = "subtitle";
+//   pSubtitle.textContent = subtitle;
+//   document.querySelectorAll(".priorityLevel").forEach((div) => {
+//     if (div.textContent === "high") {
+//       div.className = "priorityLevelHigh";
+//     } else {
+//       div.className = "priorityLevelLow";
+//     }
+//   });
 
-  todoListContainer.appendChild(priorityDiv);
+//   const priorityDiv = document.createElement("div");
+//   priority == "high"
+//     ? (priorityDiv.className = "priorityLevelHigh")
+//     : (priorityDiv.className = "priorityLevelLow");
+//   priorityDiv.textContent = priority.replace("-", " ");
 
-  li.appendChild(todoListContainer);
+//   titleSubtitle.appendChild(pTitle);
+//   titleSubtitle.appendChild(pSubtitle);
+//   todoListContainer.appendChild(titleSubtitle);
 
-  // Append the new task to the project container
-  ul.appendChild(li);
+//   todoListContainer.appendChild(priorityDiv);
 
-  // Optional: Clear form fields after adding the task
-  document.getElementById("projectTitle").value = "";
-  document.getElementById("projectSubtitle").value = "";
-  radios.forEach((radio) => (radio.checked = false));
-});
+//   li.appendChild(todoListContainer);
+
+//   // Append the new task to the project container
+//   ul.appendChild(li);
+
+//   // Optional: Clear form fields after adding the task
+//   document.getElementById("projectTitle").value = "";
+//   document.getElementById("projectSubtitle").value = "";
+//   radios.forEach((radio) => (radio.checked = false));
+// });
